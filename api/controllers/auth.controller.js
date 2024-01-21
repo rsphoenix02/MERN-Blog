60f749156c95f18d368eb70075358a929f9a00e3
rsphoenix02 <saq.saf0501@gmail.com>
2024-01-21 13:40:22 +0530

    added delete user api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -35,7 +35,7 @@ export const signin = async (req, res, next) => {
     try {
         const validUser = await User.findOne({email});
         if(!validUser){
-            return next(errorHandler(400, 'Wrong Credentials'));
+            return next(errorHandler(404, 'User Not Found'));
         }
 
         const validPassword = bcryptjs.compareSync(password, validUser.password);
